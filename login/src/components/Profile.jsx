179c5afe461370d9c9b9b34d73ef17203946cf3f
mohammad-salman0 <salmanmohammad339@gmail.com>
2026-06-21 18:30:38 +0530

    implemented log off feature

--- login/src/components/Profile.jsx
+++ login/src/components/Profile.jsx
@@ -2,9 +2,14 @@ import axios from "axios";
 import { useState } from "react";
 export default function Profile() {
   let [userData, setUserData] = useState();
+
+  let handleLogOut = () => {
+    alert("succesfully logged off");
+    localStorage.removeItem("token");
+    setUserData();
+  };
   let getProfileData = () => {
     let token = JSON.parse(localStorage.getItem("token"));
-    console.log(token);
     let config = {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -14,18 +19,19 @@ export default function Profile() {
     axios
       .get("https://api.escuelajs.co/api/v1/auth/profile", config)
       .then((res) => {
-        console.log(res);
+        alert("user details fetched");
         setUserData(res.data);
       })
       .catch((err) => {
         console.log(err);
+        alert("you are not logged in");
       });
   };
   return (
     <div>
       <p>Profile</p>
       <button onClick={getProfileData}>get Profile Data</button>
-      {/* <button onClick={handleLogOut}>Log Out</button> */}
+      <button onClick={handleLogOut}>Log Out</button>
 
       {userData && (
         <div>
