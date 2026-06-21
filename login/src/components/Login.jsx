5b8549e94507258023a5695f974d8bac727af2a2
mohammad-salman0 <salmanmohammad339@gmail.com>
2026-06-21 17:12:44 +0530

    creating login page component

--- login/src/components/Login.jsx
+++ login/src/components/Login.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 export default function Login() {
+  let [email, setEmail] = useState();
+  let [password, setPassword] = useState();
+
+  let handleSubmit = () => {
+    let payload = {
+      email: email,
+      password: password,
+    };
+    setEmail("");
+    setPassword("");
+    console.log(payload);
+  };
   return (
     <div>
       <p>Login Page</p>
+
+      <p>Email:</p>
+      <input
+        type="email"
+        value={email}
+        placeholder="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <p>Password:</p>
+      <input
+        type="password"
+        value={password}
+        placeholder="Enter password"
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+      <button onClick={handleSubmit}> Login</button>
     </div>
   );
 }
