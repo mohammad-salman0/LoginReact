8cedbb4fee1da7079d94cc63f3fce30029f1db68
mohammad-salman0 <salmanmohammad339@gmail.com>
2026-06-27 17:13:20 +0530

    creating controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,28 @@
 import userModel from "../models/user.model";
+import crypto from "crypto";
 // the below function defines the fucntion of the api end point
 // /api/auth/register
-async function register(req, res) {
+export async function register(req, res) {
   const [username, email, password] = req.body; //extracting data from request
   // checking if the user already exists as usrname and email are unique as per the defined schema
   const isAlreadyRegistered = await userModel.findOne({
     $or: [username, email],
+  });
+  if (isAlreadyRegistered) {
+    res.status(409).json({
+      message: "username or email already exits",
+    });
+  }
+  // creating hash password to be saved in the database
+  const hashedPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+
+  //storing data in the database
+  const user = userModel.create({
+    username,
+    email,
+    password: hashedPassword,
   });
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import * as authController from "./controllers/auth.controller.js";
 
 const authRouter = Router(); // is used for creating api end points
 
 // router helps us to group api end points for a similar feature, category
 
 //  whole api end point is /api/auth/register because of the prefix in app.js
-authRouter.post("/register");
+authRouter.post("/register", authController.register);
 
 export default authRouter;
