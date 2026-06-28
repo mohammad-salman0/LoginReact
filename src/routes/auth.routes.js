d3a55d59b075de673359b4cc8070cdea4967dfc0
mohammad-salman0 <salmanmohammad339@gmail.com>
2026-06-28 22:28:23 +0530

    registration part complete

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,4 +8,6 @@ const authRouter = Router(); // is used for creating api end points
 //  whole api end point is /api/auth/register because of the prefix in app.js
 authRouter.post("/register", authController.register);
 
+// registraion part complete
+
 export default authRouter;
