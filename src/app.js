5832094e166308ec822ec5236eb8a40601845547
mohammad-salman0 <salmanmohammad339@gmail.com>
2026-06-28 22:29:01 +0530

    registration part complete

--- src/app.js
+++ src/app.js
@@ -8,4 +8,6 @@ const app = express();
 app.use(express.json()); //used for parsing
 app.use(morgan("dev")); // logger :-> records details about the request sent
 app.use("/api/auth", authRouter); // /api/auth is the prefix for every request assosiated with authRouter api end point
+
+// registration part complete.
 export default app;
